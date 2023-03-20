9cfc13edf8251e2320434ef6c2b634491059a5cb
Benson Njuguna <njugunab655@gmail.com>
2023-03-20 17:59:41 +0300

    Add books to list and remove books from list

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-const form = document.querySelector("#book-form");
-
+const form = document.querySelector('#book-form');
+const bookListContainer = document.querySelector('#book-list');
 const books = [];
 
+// create a book obj
+function BookObj(title, author) {
+    this.title = title;
+    this.author = author;
+}
 
-
+// add a book to books array
 function addBook(event) {
     event.preventDefault();
     const title= event.target[0].value;
     const author= event.target[1].value;
-    console.log(title,author)
+    const newBook = new BookObj(title, author);
+    books.push(newBook);
+    displayBooks(newBook, books.length - 1);
 }
-form.addEventListener("submit",addBook);
+
+// delete a book
+function removeBook(index) {
+    books.splice(index, 1);
+    bookListContainer.innerHTML = '';
+    books.forEach((book, index) => {
+        displayBooks(book, index)
+    });
+}
+
+// display books
+function displayBooks({
+    title, author,
+}, index) {
+    const li = document.createElement('li');
+    li.textContent = `${title} by ${author} `;
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {removeBook(index)});
+    li.appendChild(removeBtn);
+    bookListContainer.appendChild(li);
+}
+
+form.addEventListener('submit',addBook);
