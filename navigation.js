5fbb7b6eb546ef6f6702ffaec9e7104166e73328
Benson Njuguna <njugunab655@gmail.com>
2023-03-22 18:03:56 +0300

    Fix eslint errors

--- navigation.js
+++ navigation.js
@@ -1,19 +1,21 @@
-const sections = document.querySelectorAll("section");
-const bookListSection = document.querySelector("#book-list-section");
-const navBtns = document.querySelectorAll(".nav-btn a");
+const sections = document.querySelectorAll('section');
+const bookListSection = document.querySelector('#book-list-section');
+const navBtns = document.querySelectorAll('.nav-btn a');
 
 function renderPage(e) {
-  sections.forEach((section) => section.style.display = "none")
+  sections.forEach((section) => {
+    section.style.display = 'none';
+  });
   const btn = e.target;
-  const idOfClicked = btn.getAttribute("data-id");
+  const idOfClicked = btn.getAttribute('data-id');
   const section = document.querySelector(`#${idOfClicked}`);
-  section.style.display = "flex"
+  section.style.display = 'flex';
 }
 
 navBtns.forEach((each) => {
-  each.addEventListener("click", renderPage)
+  each.addEventListener('click', renderPage);
 });
 
 window.onload = () => {
-  bookListSection.style.display = "flex";
-}
+  bookListSection.style.display = 'flex';
+};
