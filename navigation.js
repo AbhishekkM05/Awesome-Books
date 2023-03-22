89b1f8b09b1cf80345b999320bd575c5a4ca5c5d
Benson Njuguna <njugunab655@gmail.com>
2023-03-22 18:30:55 +0300

    Style navbtns on click

--- navigation.js
+++ navigation.js
@@ -6,10 +6,14 @@ function renderPage(e) {
   sections.forEach((section) => {
     section.style.display = 'none';
   });
+  navBtns.forEach((btn) => {
+    btn.classList.remove("btn-active")
+  })
   const btn = e.target;
   const idOfClicked = btn.getAttribute('data-id');
   const section = document.querySelector(`#${idOfClicked}`);
   section.style.display = 'flex';
+  btn.classList.toggle("btn-active")
 }
 
 navBtns.forEach((each) => {
@@ -18,4 +22,5 @@ navBtns.forEach((each) => {
 
 window.onload = () => {
   bookListSection.style.display = 'flex';
+  Array.from(navBtns)[0].className = "btn-active"
 };
